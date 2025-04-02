02122e52c524a46225215f487412895dfcb9539a
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-04-03 00:42:06 +0900

    レンダリングを表示するように修正

--- src/app/blog-detail/post-detail.component.ts
+++ src/app/blog-detail/post-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { PostService, Post } from '../post.service';
+import { RenderInfoService } from '../render-info.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -9,10 +10,14 @@ import { PostService, Post } from '../post.service';
   templateUrl: './post-detail.component.html',
 })
 export class PostDetailComponent implements OnInit {
-  post: Post | null = null;
+  private readonly route = inject(ActivatedRoute);
+  private readonly renderInfoService = inject(RenderInfoService);
+  private readonly postService = inject(PostService);
 
-  private route = inject(ActivatedRoute);
-  private postService = inject(PostService);
+  post: Post | null = null;
+  renderingMode = this.renderInfoService.getRenderingMode();
+  fetchTime = new Date();
+  isServer = this.renderInfoService.isServer();
 
   ngOnInit() {
     const postId = this.route.snapshot.paramMap.get('id');
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PostService, Post } from '../post.service';
+import { RenderInfoService } from '../render-info.service';
 
 @Component({
   selector: 'app-post-list',
@@ -9,9 +10,13 @@ import { PostService, Post } from '../post.service';
   templateUrl: './post-list.component.html',
 })
 export class PostListComponent implements OnInit {
-  posts: Post[] = [];
+  private readonly postService = inject(PostService);
+  private readonly renderInfoService = inject(RenderInfoService);
 
-  private postService = inject(PostService);
+  posts: Post[] = [];
+  renderingMode = this.renderInfoService.getRenderingMode();
+  isServer = this.renderInfoService.isServer();
+  generatedAt = new Date();
 
   ngOnInit() {
     this.postService.getPosts().subscribe((data) => {
