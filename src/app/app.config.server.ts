e2be2cb8e912e5383dedfbb32dbe02cc352f6e8f
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-04-06 00:04:46 +0900

    ISRの実装

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,12 +3,14 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { provideISR } from '@rx-angular/isr/server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
-  ]
+    provideServerRouting(serverRoutes),
+    provideISR(),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
